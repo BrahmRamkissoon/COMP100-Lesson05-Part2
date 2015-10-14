e57a5e92be7a0b3397e8f3195c58a76036d222e4
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-13 21:51:30 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP100_Lesson05_Part2
+{
+    class Program
+    {
+        enum WeekDay { SUNDAY=1, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY };
+
+        const double HST = 0.13d;
+
+        static void Main ( string [] args )
+        {
+            int age;
+            int day;
+
+            day = (int)WeekDay.SUNDAY;
+            Console.WriteLine( "Enter your age: " );
+            age = Convert.ToInt32( Console.ReadLine() );
+
+
+            Console.WriteLine( (int)WeekDay.SUNDAY);
+
+            Console.WriteLine( "Press any key to exit..." );
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
